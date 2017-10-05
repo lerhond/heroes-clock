a07db2ad1a58f8f487c24bf1bdf027309c1615b8
Lerhond <lerhond@gmail.com>
2017-10-05 11:15:08 +0200

    Basic timer logic

--- heroes-clock.py
+++ heroes-clock.py
@@ -6,8 +6,8 @@ hotkey = 'F2'
 
 
 config = {
+    'before_game_text': 'Press {hotkey} when the gates open (at 0:00 game time).',
     'Infernal Shrines': {
-        # 'before_game_text': 'Press {hotkey} when the gates open.',
         'first_spawn_time': 115,
         'next_spawn_time': 115,
         'timer_text': 'The Shrine will spawn in',
@@ -18,11 +18,16 @@ config = {
 
 battleground = None
 current_state = None
-last_time = None
+count_until = None
 
 
-def key_pressed():
-    pass
+def key_pressed(bg_config):
+    global count_until
+    count_until = time.time() + bg_config['next_spawn_time']
+
+
+def format_time(t):
+    return '{m:.0f}:{s:02.0f}'.format(m=t//60, s=t%60)
 
 
 if __name__ == '__main__':
@@ -30,8 +35,15 @@ if __name__ == '__main__':
     bg_config = config[battleground]
     print('Press {hotkey} when the gates open.'.format(hotkey=hotkey))
     keyboard.wait(hotkey)
-    last_time = time.time()
-    current_state = 'first_countdown'
-    keyboard.add_hotkey(hotkey, key_pressed)
+    count_until = time.time() + bg_config['first_spawn_time']
+    keyboard.add_hotkey(hotkey, key_pressed, args=(bg_config,))
     while True:
+        time_left = count_until - time.time()
+        if time_left > 0:
+            print('{timer_text} {time}'.format(
+                timer_text=bg_config['timer_text'],
+                time=format_time(time_left)))
+        else:
+            print(bg_config['during_objective_text'])
+            print(bg_config['hotkey_prompt'].format(hotkey=hotkey))
         time.sleep(1)
